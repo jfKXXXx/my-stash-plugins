16086513644010bfa4a07b648e176797d665b0f5
jfKXXXx <johnfkennedyxxx@gmail.com>
2025-10-23 13:43:24 +0200

    fix: reorder console log and return statement in CONFIG function

--- plugins/PicsWall/javascript.js
+++ plugins/PicsWall/javascript.js
@@ -3,7 +3,7 @@
 
   const CONFIG = async () => {
     const conf = await pluginApi.getConfiguration("PicsWall");
-    return conf ?? null;
     console.log("plugin config:", conf);
+    return conf ?? null;
   };
 };
