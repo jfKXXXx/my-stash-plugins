fe7386607adc0c69e68635d12c59939091a5b74e
jfKXXXx <johnfkennedyxxx@gmail.com>
2025-10-23 19:06:19 +0200

    fix: correct console log placement in getConfig function

--- plugins/PicsWall/javascript.js
+++ plugins/PicsWall/javascript.js
@@ -1,26 +1,24 @@
-() => {
-  "use strict";
+"use strict";
 
-  const App = {
-    config: null,
+const App = {
+  config: null,
 
-    pluginInitialized: false,
-  };
-
-  const getConfig = async () => {
-    try {
-      const config = await getConfiguration("PicsWall");
-      return config ?? null;
-    } catch (e) {
-      console.error("PicsWall: Failed to get configuration", e);
-    }
-    console.log("PicsWall: Configuration loaded", config);
-  };
-
-  //Initialization
-  () => {
-    if (App.pluginInitialized) return;
-    App.config = getConfig();
-    App.pluginInitialized = true;
-  };
+  pluginInitialized: false,
 };
+
+const getConfig = async () => {
+  try {
+    const config = await getConfiguration("PicsWall");
+    App.config = config ?? null;
+    if (!App.config) {
+      throw new Error("No configuration found");
+    }
+    return App.config;
+  } catch (e) {
+    console.error("PicsWall: Failed to get configuration", e);
+  }
+  console.log("PicsWall: Configuration loaded", config);
+};
+
+//Initialization
+let conf = await getConfig(); //test
