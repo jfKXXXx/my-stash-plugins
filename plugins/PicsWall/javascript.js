664c6db994d61e380e8f633c724c2e1e04e8e1fd
jfKXXXx <johnfkennedyxxx@gmail.com>
2025-10-23 14:07:34 +0200

    refactor: restructure configuration loading and initialization logic in PicsWall plugin

--- plugins/PicsWall/javascript.js
+++ plugins/PicsWall/javascript.js
@@ -1,9 +1,26 @@
 () => {
   "use strict";
 
-  const CONFIG = async () => {
-    const conf = await pluginApi.getConfiguration("PicsWall");
-    console.log("plugin config:", conf);
-    return conf ?? null;
+  const App = {
+    config: null,
+
+    pluginInitialized: false,
+  };
+
+  const getConfig = async () => {
+    try {
+      const config = await getConfiguration("PicsWall");
+      App.config = config ?? null;
+    } catch (e) {
+      console.error("PicsWall: Failed to get configuration", e);
+    }
+    console.log("PicsWall: Configuration loaded", CONFIG);
+  };
+
+  //Initialization
+  async () => {
+    await getConfig();
+    App.pluginInitialized = true;
+    console.log("PicsWall: Plugin initialized");
   };
 };
