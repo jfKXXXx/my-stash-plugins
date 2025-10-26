8d55ac7e2efaa3cd146d692ae7cdb0d099221d8e
jfKXXXx <johnfkennedyxxx@gmail.com>
2025-10-27 00:37:14 +0100

    reset

--- plugins/PicsWall/javascript.js
+++ plugins/PicsWall/javascript.js
@@ -1,107 +1,26 @@
 "use strict";
 
-const App = {
-  config: {},
-  pluginInitialized: false,
-};
+function main() {
 
-const getPluginConfig = async () => {
-  try {
-    console.log("PicsWall Building config query...")
-    const query = `query GetPluginConfig {
-   configuration {
-    general
-    {
-    	apiKey
-      username
-      password
-      
-      }
-        ui
-        plugins
-      
-    }
-    }`
-      
+  if (!document.location.pathname.includes("/images/")) return;
+
+  csLib.waitForElement(".mb2 btn-group", (elt) => {
+    console.log("PicsWall: btn group html found", elt);
+    CreatePicsWallButton(elt);
     
-      console.log("Sending graphql request: ", query)
-      console.log("Waiting response...")
-      await csLib.callGQL({query}).then(res => {
-        console.log("PicsWall: graphql response found: ", res);
-        App.config = {
-          apiKey: res.configuration.general.apiKey,
-          pluginConfig: res.configuration?.plugins["PicsWall"] ?? {}
-
-        };
-
-    })
-    
-
-    if (!App.config) {
-      console.warn("PicsWall: No configuration found");
-      return null;
-    }
-    console.log("PicsWall: Configuration loaded", App.config);
-
-    return App.config;
-  } catch (e) {
-    console.error("PicsWall: Failed to get configuration", e);
-    return null;
-  }
-};
-
-function OnStashPageChange(evt) {
-  console.log("PicsWall: Stash location changed event fired:", evt)
-  console.log("PicsWall: Stash current page", document.location.href)
+  
+  })
 
 
 }
 
-function OnDomReady(evt){
-  console.log("PicsWall: DOMLoaded event fired:", evt)
-  console.log("PicsWall: creating event listeners...");
-
-  
-    
-  
+function CreatePicsWallButton(elt) {
+  const button = document.createElement("button");
+  button.innerHTML = `<i class="fa-solid fa-images"></i>`
+  button.onclick = () => {
+    console.log("PicsWall button clicked");
+  };
+  elt.appendChild(button);
 }
 
-async function InitPlugin() {
-  try {
-     console.log("Initialising PicsWall Plugin...");
-     if (App.pluginInitialized) {
-        console.warn("PicsWall plugin already initialised: exiting.");
-        return null;
-
-      }
-      console.log("Getting plugin configuration...");
-      await getPluginConfig().then(data => {
-          console.log("Found configuration data: ", data);
-        })
-
-      // Create event listeners...
-      console.log("waiting for dom loading...");
-      
-      const elt = await csLib.waitForElement(".mb2 btn-group", OnDomReady);
-      
-    console.log("PicsWall plugin initialized successfully.") //TODO: send custom event ?
-    App.pluginInitialized= true;
-
-      
-
-  }
-  catch (error) {
-    console.error("error during PicsWall plugin Init: ", error)
-
-  }
-  
- 
-  
-}
-
-
-
-// Initialization (avoid top-level await by using an async IIFE)
-(async () => {
-  InitPlugin();
-})();
+main();
