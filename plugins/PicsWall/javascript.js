8d8ba99183ed77233fcb56a361e626d6cfc59e46
jfKXXXx <johnfkennedyxxx@gmail.com>
2025-10-27 21:28:39 +0100

    refactor

--- plugins/PicsWall/javascript.js
+++ plugins/PicsWall/javascript.js
@@ -1,4 +1,7 @@
 "use strict";
+let wallActive = false;
+let picsWall = document.createElement("div");
+picsWall.className = "picswall";
 
 function main() {
 
@@ -6,23 +9,29 @@ function main() {
 
   csLib.waitForElement(".filtered-list-toolbar.btn-toolbar", (elt) => {
     console.log("PicsWall: images toolbarr html found", elt);
-    CreatePicsWallButton(elt);
+    const wallbtn= CreatePicsWallButton(elt);
+    wallbtn.addEventListener('click', OnPicwallClick);
     
-  
   })
+}
 
-
+function OnPicwallClick(e) {
+  console.log("PicsWall button clicked");
+  e.preventDefault();
+  e.target.value = "active";
+  e.target.classList.toggle("active");
+  wallActive = e.target.value === "active";
+  console.log("PicsWall button is now", wallActive ? "active" : "inactive");
 }
 
 function CreatePicsWallButton(elt) {
   console.log("PicsWall: creating button on :", elt);
   const button = document.createElement("button");
   button.innerHTML = `<i class="fa-solid fa-images"></i>`
-  button.className = "btn-active btn-secondary"
-  button.onclick = () => {
-    console.log("PicsWall button clicked");
-  };
+  button.className = "btn-active btn-secondary picswall-btn"
+  
   elt.appendChild(button);
+  return button;
 }
 
 main();
